f532311b79eb9c70dda52f488384ca8a544e42ed
Nijah1797 <ngoolsb2@students.kennesaw.edu>
2026-04-27 21:41:53 -0400

    Update Nijah's Text Adventure Game.py

--- Nijah's Text Adventure Game.py
+++ Nijah's Text Adventure Game.py
@@ -4,3 +4,4 @@ user_name = input("What is your name? ")
 print(f"Hello {user_name}, you wake up with a letter from the princess kingdom asking you to help them rescue their princess.")
 
 user_choice = input("Do you choose to accept it?").lower()
+print(user_choice)
